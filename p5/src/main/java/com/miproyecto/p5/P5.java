2c9dcda11f624fc21279e2c648f8273ffcb18285
Ana Belen <Ana Belen@DESKTOP-5JL2VR6>
2024-01-10 10:40:01 +0100

    commit añadir p4 java

---
+++ p5/src/main/java/com/miproyecto/p5/P5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.miproyecto.p5;
+
+/**
+ *
+ * @author Ana Belen
+ */
+public class P5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hllo World!");
+    }
+}
